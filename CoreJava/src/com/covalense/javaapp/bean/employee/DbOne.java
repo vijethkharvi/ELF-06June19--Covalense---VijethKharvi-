2b10bd486e50eafeffd9db53db22d63ab8a35262
vijeth <kharvivijeth31@gmail.com>
2019-06-26 18:06:56 +0530

    bean/corejava

---
+++ CoreJava/src/com/covalense/javaapp/bean/employee/DbOne.java
@@ -0,0 +1,14 @@
+package com.covalense.javaapp.bean.employee;
+
+import lombok.extern.java.Log;
+
+@Log
+public class DbOne {
+	void recive(Employee p) {
+		log.info("******DB1********");
+		log.info("name is " + p.getName1());
+		log.info("salary is " + p.getSalary());
+		log.info("gender is " + p.getGender());
+
+	}
+}
